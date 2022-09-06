f3efd58cdc877dcf77d7db33f6c1bc6a63a91763
fad16papa <fad16papa@gmail.com>
2022-09-07 01:52:12 +0800

    Commit changes

--- Models/RespnoseModel.cs
+++ Models/RespnoseModel.cs
@@ -10,16 +10,32 @@ namespace LambdaBot.Models
         internal class TranslateRequest
         {
             public string Text { get; init; }
+
+            public TranslateRequest()
+            {
+                Text = string.Empty;
+            }
         }
 
         public class TranslateContent
         {
             public string Translated { get; init; }
+
+            public TranslateContent()
+            {
+                Translated = string.Empty;
+            }
         }
 
         public class TranslateResponse
         {
             public TranslateContent Contents { get; init; }
+
+            public TranslateResponse()
+            {
+                TranslateContent translateContent = new TranslateContent();
+                Contents = translateContent;
+            }
         }
     }
 }
